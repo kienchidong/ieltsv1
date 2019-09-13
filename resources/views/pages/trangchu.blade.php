03296d15f65d1037cd2541967ec44f5039e8f64e
TRANCONGVU <vndvip10>
2019-09-13 17:25:12 +0700

    trang chu

--- resources/views/pages/trangchu.blade.php
+++ resources/views/pages/trangchu.blade.php
@@ -27,6 +27,17 @@
         </div>
     </div>
 </section>
+<section class="section-11">
+    <div class="container-fluid">
+        <div class="container work-shop">
+            <div>
+                <span>The Ilest</span><span>Work Shop</span><br>
+                <a href="">Xem Thêm</a>
+            </div>
+
+        </div>
+    </div>
+</section>
 <section class="section-2">
     <div class="container-fluid">
         <div class="container">
@@ -54,7 +65,7 @@
 
                     </div>
                 </div>
-                <div class="col-lg-4 col-md-6 col-sm-6" >
+                <div class="col-lg-4 col-md-6 col-sm-6">
                     <div class="course-box">
                         <img class="img-responsive" src="image/course.jpg" alt="">
                         <div class="overlay">
@@ -155,16 +166,42 @@
                 <div class="col-md-6 library-left">
                     <h5 class="section-title">Thư viện Offline</h5>
                     <div class="tree-skill">
-                            <img class="tree" src="image/tree.png" alt="">
-                            <div class="skil-1">
+                        <img class="tree" src="image/tree.png" alt="">
+                        <div class="skill">
+                            <a>
+                                Khóa rễ
+                            </a>
+                        </div>
+                        <div class="skill skill-2">
+                            <a>
+                                Khóa gốc
+                            </a>
+                        </div>
+                        <div class="skill skill-3">
+                            <a>
+                                Khóa thân
+                            </a>
+                        </div>
+                        <div class="skill skill-4">
+                            <a>
+                                Khóa ngọn
+                            </a>
+                        </div>
 
-                            </div>
                     </div>
 
                 </div>
                 <div class="col-md-6 library-right">
-                        <h5 class="section-title">Thư viện Online</h5>
-
+                    <h5 class="section-title">Thư viện Online</h5>
+                    <div class="row">
+                        <div class="col-md-12 mt-5">
+                            <iframe width="100%" height="315" src="https://www.youtube.com/embed/VBBrkpDp_3U"
+                                frameborder="0"
+                                allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                                allowfullscreen></iframe>
+                            <a class="dk-online btn btn-outline-secondary" href="">Đăng ký ngay</a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
@@ -174,14 +211,311 @@
     <div class="container-fluid">
         <div class="container">
             <div class="row">
+                <div class="col-md-3 statistic">
+                    <i class="fa fa-book fa-5x"></i>
+                    <span>2228</span>
+                    <span>E-Book Được bán</span>
+                </div>
+                <div class="col-md-3 statistic">
+                    <i class="fa fa-briefcase fa-5x"></i>
+                    <span>20</span>
+                    <span>Khóa học</span>
+                </div>
+                <div class="col-md-3 statistic">
+                    <i class="fa fa-calendar-check fa-5x"></i>
+                    <span>2228</span>
+                    <span>Học viên</span>
+                </div>
+                <div class="col-md-3 statistic">
+                    <i class="fa fa-graduation-cap fa-5x"></i>
+                    <span>2228</span>
+                    <span>Học viên đạt 6.5 Ilest</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="section-5">
+    <div class="container-fluid">
+        <div class="container">
+            <h4 class="section-title">Giáo Viên Ielst</h4>
+            <div class="row teacher">
                 <div class="col-md-3">
+                    <div class="avatar-teacher">
+                        <img src="image/1.png" alt="">
+                    </div>
+                </div>
+                <div class="col-md-9 d-flex flex-column">
+                    <span class="teacher-name">
+                        <span> Thầy : Trần Hồng Dũng</span>
+                    </span>
+                    <span class="teacher-info">
+                        Đạt điểm Ielst 8.5 Năm 2019
+                    </span>
+                    <span class="teacher-info">
+                        Giảng viên đại học quốc gia Việt Nam
+                    </span>
+                    <span class="teacher-info">
+                        Du học Anh từ 2010 - 2018
+                    </span>
+                    <span>Thông tin thêm Facebook : <a href="">Trần Hồng Dũng</a></span>
+                </div>
+            </div>
+            <div class="row teacher">
+                <div class="col-md-3 order-2">
+                    <div class="avatar-teacher">
+                        <img src="image/1.png" alt="">
+                    </div>
+                </div>
+                <div class="col-md-9 order-1 d-flex flex-column">
+                    <span class="teacher-name">
+                        <span> Thầy : Trần Hồng Dũng</span>
+                    </span>
+                    <span class="teacher-info">
+                        Đạt điểm Ielst 8.5 Năm 2019
+                    </span>
+                    <span class="teacher-info">
+                        Giảng viên đại học quốc gia Việt Nam
+                    </span>
+                    <span class="teacher-info">
+                        Du học Anh từ 2010 - 2018
+                    </span>
+                    <span>Thông tin thêm Facebook : <a href="">Trần Hồng Dũng</a></span>
+                </div>
+            </div>
+            <div class="row teacher">
+                <div class="col-md-3">
+                    <div class="avatar-teacher">
+                        <img src="image/1.png" alt="">
+                    </div>
+                </div>
+                <div class="col-md-9 d-flex flex-column">
+                    <span class="teacher-name">
+                        <span> Thầy : Trần Hồng Dũng</span>
+                    </span>
+                    <span class="teacher-info">
+                        Đạt điểm Ielst 8.5 Năm 2019
+                    </span>
+                    <span class="teacher-info">
+                        Giảng viên đại học quốc gia Việt Nam
+                    </span>
+                    <span class="teacher-info">
+                        Du học Anh từ 2010 - 2018
+                    </span>
+                    <span>Thông tin thêm Facebook : <a href="">Trần Hồng Dũng</a></span>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="section-6 pb-4">
+    <div class="container-fluid">
+        <div class="container">
+            <h4 class="section-title">
+                Blog Tiếng anh
+            </h4>
+            <div class="blog">
+                <div class="d-flex justify-content-between">
+                    <a href="#" class="title-blog">giải đáp thắc mắc về kỳ thi Ielst</a>
+                    <div class="time-blog">
+                        <i class="fa fa-calendar-alt"></i>
+                        <span>13 Stb 2019</span>
+                    </div>
+                </div>
+                <div class="blog-content">
+                    <div class="blog-img">
+                        <img src="image/blog1.png" class="img-reponsive" alt="">
+                    </div>
+                    <span>Có bao giờ bạn tự hỏi Ielst là gì ? Làm thế nào để thi ielst
+                        ? Điểm trong ielset được tính như thế nào .... nếu bạn là người mới bắt đầu
+                        chắc chắn sẽ gặp những thắc mắc như vậy . Bài viết này sẽ giúp bạn hiểu phần
+                        nào về Ielst
+                    </span>
+                    <a href="" class="doctiep btn btn-outline-danger">
+                        Đọc tiếp
+                    </a>
+                </div>
+                <div class="border-bottom">
+
+                </div>
+            </div>
+            <div class="blog">
+                <div class="d-flex justify-content-between">
+                    <a href="#" class="title-blog">giải đáp thắc mắc về kỳ thi Ielst</a>
+                    <div class="time-blog">
+                        <i class="fa fa-calendar-alt"></i>
+                        <span>13 Stb 2019</span>
+                    </div>
+                </div>
+                <div class="blog-content">
+                    <div class="blog-img">
+                        <img src="image/blog1.png" class="img-reponsive" alt="">
+                    </div>
+                    <span>Có bao giờ bạn tự hỏi Ielst là gì ? Làm thế nào để thi ielst
+                        ? Điểm trong ielset được tính như thế nào .... nếu bạn là người mới bắt đầu
+                        chắc chắn sẽ gặp những thắc mắc như vậy . Bài viết này sẽ giúp bạn hiểu phần
+                        nào về Ielst
+                    </span>
+                    <a href="" class="doctiep btn btn-outline-danger">
+                        Đọc tiếp
+                    </a>
+                </div>
+                <div class="border-bottom">
+
+                </div>
+            </div>
+            <div class="blog">
+                <div class="d-flex justify-content-between">
+                    <a href="#" class="title-blog">giải đáp thắc mắc về kỳ thi Ielst</a>
+                    <div class="time-blog">
+                        <i class="fa fa-calendar-alt"></i>
+                        <span>13 Stb 2019</span>
+                    </div>
+                </div>
+                <div class="blog-content">
+                    <div class="blog-img">
+                        <img src="image/blog1.png" class="img-reponsive" alt="">
+                    </div>
+                    <span>Có bao giờ bạn tự hỏi Ielst là gì ? Làm thế nào để thi ielst
+                        ? Điểm trong ielset được tính như thế nào .... nếu bạn là người mới bắt đầu
+                        chắc chắn sẽ gặp những thắc mắc như vậy . Bài viết này sẽ giúp bạn hiểu phần
+                        nào về Ielst
+                    </span>
+                    <a href="" class="doctiep btn btn-outline-danger">
+                        Đọc tiếp
+                    </a>
+                </div>
+                <div class="border-bottom">
+
+                </div>
+            </div>
+            <div class="blog">
+                <div class="d-flex justify-content-between">
+                    <a href="#" class="title-blog">giải đáp thắc mắc về kỳ thi Ielst</a>
+                    <div class="time-blog">
+                        <i class="fa fa-calendar-alt"></i>
+                        <span>13 Stb 2019</span>
+                    </div>
+                </div>
+                <div class="blog-content">
+                    <div class="blog-img">
+                        <img src="image/blog1.png" class="img-reponsive" alt="">
+                    </div>
+                    <span>Có bao giờ bạn tự hỏi Ielst là gì ? Làm thế nào để thi ielst
+                        ? Điểm trong ielset được tính như thế nào .... nếu bạn là người mới bắt đầu
+                        chắc chắn sẽ gặp những thắc mắc như vậy . Bài viết này sẽ giúp bạn hiểu phần
+                        nào về Ielst
+                    </span>
+                    <a href="" class="doctiep btn btn-outline-danger">
+                        Đọc tiếp
+                    </a>
+                </div>
+                <div class="border-bottom">
+
+                </div>
+            </div>
+            <a href="" class="btn btn-outline-danger">
+                Xem thêm blog
+            </a>
+        </div>
+    </div>
+</section>
+<section class="section-7">
+    <div class="container-fluid">
+        <div class="container danhgia">
+            <h4 class="section-title text-white">Cảm nhận của học viên về Xuan Phi Ielst</h4>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="coment-box">
+                        <div class="comment-title">
+                            Em chả biết nói gì ngoài việc e rất yêu cô Thủy :)) học hết khóa đầu rất nhớ cô, chỉ muốn đi
+                            học cô tiếp
+                            :( Cô rất tận tình nè, hiền nè, xinh nè, giảng hay dễ hiểu nè, khiến e ko sợ đi học nữa, cô
+                            cũng buồn cười nữa =)
+                        </div>
+                        <div class="coment-member">
+                            <div class="comment-avt">
+                                <img src="https://photo-3-baomoi.zadn.vn/w1000_r1/2018_10_03_180_28002521/27ad3ab469f580abd9e4.jpg"
+                                    alt="">
+                            </div>
+                            <div class="coment-name">
+                                <a href="">Hoàng Thu Hà</a> <br>
+                                <span>Khóa Ielst-01</span>
+                                <div class="star">
+                                    <i class="fa fa-star star-check"></i>
+                                    <i class="fas fa-star star-check"></i>
+                                    <i class="fas fa-star star-check"></i>
+                                    <i class="fas fa-star star-check"></i>
+                                    <i class="fas fa-star star-check"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+                </div>
+                <div class="col-md-4">
+                    <div class="coment-box">
+                        <div class="comment-title">
+                            Em chả biết nói gì ngoài việc e rất yêu cô Thủy :)) học hết khóa đầu rất nhớ cô, chỉ muốn đi
+                            học cô tiếp
+                            :( Cô rất tận tình nè, hiền nè, xinh nè, giảng hay dễ hiểu nè, khiến e ko sợ đi học nữa, cô
+                            cũng buồn cười nữa =)
+                        </div>
+                        <div class="coment-member">
+                            <div class="comment-avt">
+                                <img src="https://photo-3-baomoi.zadn.vn/w1000_r1/2018_10_03_180_28002521/27ad3ab469f580abd9e4.jpg"
+                                    alt="">
+                            </div>
+                            <div class="coment-name">
+                                <a href="">Hoàng Thu Hà</a> <br>
+                                <span>Khóa Ielst-01</span>
+                                <div class="star">
+                                    <i class="fa fa-star star-check"></i>
+                                    <i class="fas fa-star star-check"></i>
+                                    <i class="fas fa-star star-check"></i>
+                                    <i class="fas fa-star star-check"></i>
+                                    <i class="fas fa-star star-check"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+
+                </div>
+                <div class="col-md-4">
+                    <div class="coment-box">
+                        <div class="comment-title">
+                            Em chả biết nói gì ngoài việc e rất yêu cô Thủy :)) học hết khóa đầu rất nhớ cô, chỉ muốn đi
+                            học cô tiếp
+                            :( Cô rất tận tình nè, hiền nè, xinh nè, giảng hay dễ hiểu nè, khiến e ko sợ đi học nữa, cô
+                            cũng buồn cười nữa =)
+                        </div>
+                        <div class="coment-member">
+                            <div class="comment-avt">
+                                <img src="https://photo-3-baomoi.zadn.vn/w1000_r1/2018_10_03_180_28002521/27ad3ab469f580abd9e4.jpg"
+                                    alt="">
+                            </div>
+                            <div class="coment-name">
+                                <a href="">Hoàng Thu Hà</a> <br>
+                                <span>Khóa Ielst-01</span>
+                                <div class="star">
+                                    <i class="fa fa-star star-check"></i>
+                                    <i class="fas fa-star star-check"></i>
+                                    <i class="fas fa-star star-check"></i>
+                                    <i class="fas fa-star star-check"></i>
+                                    <i class="fas fa-star star-check"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
 
                 </div>
             </div>
         </div>
     </div>
-</section>-
-
+</section>
 
 
 <script type="text/javascript" src="lib/jquery.min.js"></script>
