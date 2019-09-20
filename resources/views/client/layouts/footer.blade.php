14c9b47e5ff1a19c3bfc3fb22cf7f682ef1a5af0
openupmta <openupmta99@gmail.com>
2019-09-20 22:50:25 +0700

    ngu

---
+++ resources/views/client/layouts/footer.blade.php
@@ -0,0 +1,105 @@
+	<footer class="container-fluid">
+	    {{--  <div class="container-fluid footer-1">
+	        <div class="container">
+	            <div class="row">
+	                <div class="col-md-4">
+	                    <h5>LIÊN KẾT</h5>
+	                    <div class="d-flex">
+	                        <div class="icon-lk">
+	                            <img src="image/icon-fb.jpg" alt="facebook">
+	                        </div>
+	                        <div class="icon-lk">
+	                            <img src="image/icon-zalo.png" alt="zalo">
+	                        </div>
+	                        <div class="icon-lk">
+	                            <img src="image/youtube-icon.png" alt="youtube">
+	                        </div>
+	                    </div>
+	                </div>
+	                <div class="col-md-8">
+	                    <h5>ĐĂNG KÝ NHẬN THÔNG TIN </h5>
+	                    <div class="form-dk">
+	                        <form>
+	                            <div class="form-group">
+	                                <input type="text" class="form-control" id="" aria-describedby="text"
+	                                    placeholder="Nhập vào email">
+	                            </div>
+	                            <button type="button" class="btn btn-warning">Đăng Ký</button>
+	                        </form>
+
+	                    </div>
+
+	                </div>
+	            </div>
+
+	        </div>
+	    </div>  --}}
+	    <div class="container-fluid footer-2">
+	        <div class="container" id="contact">
+	            <div class="row">
+	                <div class="col-md-3 logo-footer">
+	                    <a href="">
+	                        <img src="{{asset('')}}image/logo.png" alt="">
+	                    </a>
+	                </div>
+	                <div class="col-md-4 link-lk">
+	                    <h4>LIÊN HỆ</h4>
+	                    <a href="">Địa chỉ : Tòa nhà Dream House, số 63, ngõ 136 Chùa Láng, Hà Nội </a>
+	                    <a href="">Số Điện Thoại : 096 890 7276</a>
+	                    <a href="">Email : XuanPhiIelst@gmail.com</a>
+	                </div>
+	                <div class="col-md-5 link-lk">
+	                    <h4>ĐĂNG KÝ NHẬN THÔNG TIN </h4>
+	                    <div class="form-dk">
+	                        <form>
+	                            <div class="form-group">
+	                                <input type="text" class="form-control" id="" aria-describedby="text"
+	                                    placeholder="Nhập vào số điện thoại">
+	                            </div>
+	                            <button type="button" class="btn btn-warning">Đăng Ký</button>
+	                        </form>
+
+	                    </div>
+	                </div>
+	            </div>
+	        </div>
+	    </div>
+	</footer>
+	<section class="back-to-top">
+	    <div class="back-to-top-button">
+	        <i class="fas fa-angle-double-up"></i>
+	    </div>
+	</section>
+	<section class="menu-right">
+	    <div class="d-flex flex-column">
+	        <div class="right-icon">
+	            <a href="">
+	                <div class="box-menu-right">
+	                    <img src="image/logo.png" alt="">
+	                </div>
+	                <span class="right-icon-content">Xuân Phi IELTS</span>
+	            </a>
+
+	        </div>
+	        <div class="right-icon">
+	            <a href="">
+	                <div class="box-menu-right">
+	                    <i class="fas fa-phone-square fa-2x" style="transform: rotate(90deg);"></i>
+	                </div>
+	                <span class="right-icon-content">096 890 7276</span>
+	            </a>
+
+	        </div>
+	        <div class="right-icon">
+	            <a href="">
+	                <div class="box-menu-right">
+	                    <i class="fa fa-envelope-open-text fa-2x"></i>
+	                </div>
+	                <span class="right-icon-content">xuanphiielst@gmail.com</span>
+	            </a>
+
+	        </div>
+
+	    </div>
+	</section>
+	<script type="text/javascript" src="js/backtotop.js"></script>
---
+++ resources/views/client/layouts/header.blade.php
@@ -0,0 +1,89 @@
+<header class="header container-fluid">
+
+</header>
+
+<nav class="nav-horizontal container-fluid">
+	<div class="background-header"></div>
+	<div class="nav-horizontal-container container">
+		<div class="nav-horizontal-content">
+			<div class="d-flex justify-content-center menu">
+
+				<div class="logo">
+					<a href="{{ url('/') }}">
+						<img src="{{asset('')}}image/logo11.png" alt="">
+					</a>
+
+				</div>
+				<ul class="nav-ul-lv-1">
+					<li><a href="{{ url('/course') }}">Giới thiệu</a></li>
+					<li >
+						<a href="#khoahoc" id="khoahoc-li">khóa học<i class="fa fa-sort-down"></i></a>
+						<ul class="nav-ul-lv-2">
+							<li><a href="{{ url('landing') }}">Offline</a></li>
+							<li><a href="#khoahoc">Online</a></li>
+						</ul>
+					</li>
+					<li >
+						<a href="#thuvien" id="library-li">Thư viện<i class="fa fa-sort-down"></i></a>
+
+						<ul class="nav-ul-lv-2">
+							<li><a href="{{ url('thuvien/nghe') }}">Listening</a></li>
+							<li><a href="{{ url('thuvien/noi') }}">Speacking</a></li>
+							<li><a href="{{ url('thuvien/doc') }}">Reading</a></li>
+							<li><a href="{{ url('thuvien/viet') }}">Writing</a></li>
+							{{--  <li><a href="{{ url('thuvien/total') }}">For new Member</a></li>  --}}
+						</ul>
+					</li>
+					<li><a href="#blog" id="blog-li">chia sẻ</a>
+					</li>
+					<li id="contact-li"><a href="#">liên hệ</a></li>
+				</ul>
+				<ul class="nav-ul-lv-1">
+					<li><a href="{{ url('dangky') }}"
+						   style="    border: 3px solid #19687D;
+                            border-radius: 5px;"
+						>Đăng ký</a></li>
+				</ul>
+			</div>
+			<div class="menu-mobile-button"><i class="fas fa-bars"></i></div>
+		</div>
+	</div>
+</nav>
+<script type="text/javascript" src="js/nav-horizontal.js"></script>
+
+<section class="menu-mobile">
+	<div class="menu-mobile-bg"></div>
+	<div class="menu-mobile-box">
+		<div class="menu-mobile-info">
+
+		</div>
+		<div class="menu-mobile-content">
+			<div class="menu-left">
+				<div class="menu-left-title">
+					<h3>Menu</h3>
+				</div>
+				<div class="menu-left-content">
+
+					<ul class="menu-left-ul-lv-1">
+						<li><a href="/">Giới thiệu</a></li>
+						<li>
+							<a href="#khoahoc">khóa học</a>
+
+						</li>
+						<li>
+							<a href="#thuvien">thư viện</a>
+
+						</li>
+						<li><a href="#blog">blog</a>
+
+						</li>
+						<li><a href="{{ url('lienhe') }}">liên hệ</a></li>
+					</ul>
+
+				</div>
+			</div>
+			<script type="text/javascript" src="js/menu-left-js.js"></script>
+		</div>
+	</div>
+	<script type="text/javascript" src="js/menu-mobile.js"></script>
+</section>
